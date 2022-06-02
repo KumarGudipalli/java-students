9f65ca940841a6522efe34bc4e727ee3af06312a
KumarGudipalli <kumargudipalli97@gmail.com>
2022-06-02 12:05:14 +0530

    second

--- Accountb.java
+++ Accountb.java
@@ -1,25 +1,38 @@
 package account;
 
 public class Accountb {
- private  int balance ;
+ private  int balance = 0 ;
+ public int credit() {
+	 return balance;
+ }
+ public int debit() {
+	 return balance;
+ }
+ 
      public int getCredit()
       {
-    	  return balance;
+    	  return credit();
       }
+     
+     
     public void setCredit(int b)
     {
-    	this.balance = b;
-    }
+    	if(b>0) {
+    	this.balance = b+balance;
+
+    	}
+    	}
     
     public int getDebit()
     {
-    	return balance;
+    	return debit();
     	
     }
     public void setDebit(int d)
     {
-    
+    if(d > 0)
+    {
     		this.balance = balance-d;
-    	
+    }	
     }
 }
